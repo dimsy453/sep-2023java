21234b2badc8578fd4db0526c46e395d3fe87bdf
ZVER <dimsy453@gmail.com>
2023-11-21 12:52:32 +0200

    new fail

---
+++ lesson1/lesson1.js
@@ -0,0 +1,50 @@
+let s1="hello";
+console.log(s1);
+let s2 = "owu";
+console.log(s2);
+let s3 = 'com';
+console.log(s3);
+let s4 = 'ua';
+console.log(s4);
+let n1= 1;
+console.log(n1);
+let n2 =10;
+console.log(n2);
+let n3=-999;
+console.log(n3);
+let n4=123;
+console.log(n4);
+let n5=3.14;
+console.log(n5);
+let n6=2.7;
+console.log(n6);
+let n7=16;
+console.log(n7);
+let b1=true;
+console.log(b1);
+let b2=false;
+console.log(b2);
+
+let firstName=" Дмитро ";
+let middleName = "Сергійович";
+let lastName='Щудро ';
+let person=lastName+firstName+middleName;
+console.log(person);
+
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let first_Name= prompt("first_Name");
+let middle_Name= prompt("middle_Name");
+let age= prompt("age");
+console.log((first_Name)+ ' ' + ( middle_Name ) + '' + (age));
+
+
+
+
+
